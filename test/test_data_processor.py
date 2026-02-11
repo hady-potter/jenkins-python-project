cab0a763756d2c28739f9c4da394c83868bfb8df
hady Mohamed <57275439+hady-potter@users.noreply.github.com>
2026-02-11 06:53:17 +0200

    Update test_data_processor.py

--- test/test_data_processor.py
+++ test/test_data_processor.py
@@ -8,7 +8,7 @@ class TestDataProcessor(unittest.TestCase):
     def test_process_data(self):
         data = {'age': [23, 29, 21, 18], 'height': [165, 170, 174, 168]}
         result = process_data(data)
-        self.assertEqual(result.loc['mean', 'age'], 22.759)
+        self.assertEqual(result.loc['mean', 'age'], 22.75)
 
 if __name__ == '__main__':
     unittest.main()
